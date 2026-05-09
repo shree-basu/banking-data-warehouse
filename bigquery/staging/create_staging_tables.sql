63200b97f6c5649b3d36b623bc02be7cc7c1ebe0
Shreetama <basushreetama99@gmail.com>
2026-05-09 09:00:18 +0530

    feat:add stg_transactions staging table

--- bigquery/staging/create_staging_tables.sql
+++ bigquery/staging/create_staging_tables.sql
@@ -32,3 +32,17 @@ CREATE TABLE IF NOT EXISTS 'banking_dwh.stg_merchants'(
     registered_since STRING,
     status STRING
 );
+
+CREATE TABLE IF NOT EXISTS 'banking_dwh.stg_transactions'(
+    transaction_id STRING,
+    account_id STRING,
+    merchant_id STRING,
+    date_id STRING,
+    amount FLOAT64,
+    currency STRING,
+    channel STRING,
+    transaction_type STRING,
+    status STRING,
+    ip_address STRING,
+    country_code STRING
+);
