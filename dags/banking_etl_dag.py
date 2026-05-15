4a0adb9d1d1d1fc4fdb38043e79b7ace97a2f769
Shreetama <basushreetama99@gmail.com>
2026-05-15 07:37:52 +0530

    feat: add default arguments and DAG definition

--- dags/banking_etl_dag.py
+++ dags/banking_etl_dag.py
@@ -3,3 +3,21 @@ from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQue
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
 from airflow.operator.dummy import DummyOperator
 from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'shreetama',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+with DAG(
+    dag_id = 'banking_etl_pipeline',
+    default_args = default_args,
+    description = 'End-to-end Banking Data Warehouse ETL pipeline',
+    schedule_interval = '0 2 * * *',
+    start_date = (2024,1,1),
+    catchup = False,
+    tags = ['banking', 'etl', 'bigquery']
+) as dag:
