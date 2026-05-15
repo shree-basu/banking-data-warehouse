54a3d7c4014f6be0a6af3992f81cbc396c7d3d41
Shreetama <basushreetama99@gmail.com>
2026-05-15 07:27:11 +0530

    feat: add DAG imports and dependencies

---
+++ dags/banking_etl_dag.py
@@ -0,0 +1,5 @@
+from airflow import DAG
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+from airflow.operator.dummy import DummyOperator
+from datetime import datetime, timedelta
